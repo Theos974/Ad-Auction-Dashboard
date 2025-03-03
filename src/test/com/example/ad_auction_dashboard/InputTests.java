a9758b4bde59c3c08fde37ca57f9ea313597f225
TotaledToast <totaledtoast@gmail.com>
2025-03-03 11:42:58 +0000

    Small change to input tests

--- src/test/com/example/ad_auction_dashboard/InputTests.java
+++ src/test/com/example/ad_auction_dashboard/InputTests.java
@@ -166,13 +166,13 @@ public class InputTests {
     }
     @Test
     @DisplayName("Date Validation")
-    void datevalidation(){
+    void dateValidation(){
         LogDate log1 = new LogDate("no");
         LogDate log2 = new LogDate("n/a");
         LogDate log3 = new LogDate(2025, 3, 16, 5, 28, 6);
         assertEquals("", log1.getDate(), "no is not a valid Date");
         assertEquals("n/a", log2.getDate(), "n/a is valid as an 'invalid' date");
-        assertEquals("2025-3-16 5:28:6", log3.getDate(), "Is a valid Date");
+        assertEquals("2025-03-16 05:28:06", log3.getDate(), "Is a valid Date");
     }
     @Test
     @DisplayName("ID Validation")
