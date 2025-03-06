37e3a12d76bddcc77918ab4a30f2ee0129f745ef
Theos974 <159783923+Theos974@users.noreply.github.com>
2025-03-06 16:57:04 +0000

    final deliverable 2 with working pom and jar

---
+++ src/main/java/com/example/ad_auction_dashboard/GUIStarter.java
@@ -0,0 +1,9 @@
+package com.example.ad_auction_dashboard;
+
+import com.example.ad_auction_dashboard.viewer.MainApp;
+
+public class GUIStarter {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
